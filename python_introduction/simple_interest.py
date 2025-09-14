8815e86195188a6006e52255351681d9b47c1c5b
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-09-14 06:40:11 +0300

    add simple_interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
-principal = int(input("principal value"))  # representing 1000$"
-rate  = float(input("rate value"))  # representing 5% annual interest rate"
-time = int(input("time value"))# "representing 3 years"
-interest = p * r * t # i mean interest
+principal = 1000 # representing 1000$"
+rate  = 0.05  # representing 5% annual interest rate"
+time = 3 # "representing 3 years"
+interest = principal * rate * time
 print(interest)
