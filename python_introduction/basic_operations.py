8a5c32d85a59d9f9370357fb5ecbf6b263943354
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-09-14 05:20:58 +0300

    add basic_oberration.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 5
+number2 = 10
+addition = number1 + number2
+subtraction = number2 - number1
+multiplication = number1 * number2
+print(addition)
+print(subtraction)
+print(multiplication)
