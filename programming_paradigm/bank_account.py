0f1f074f9b1fdd457748f0423035cf96da3154e5
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-10-05 21:54:11 +0300

    ADD

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,11 +1,11 @@
 class BankAccount:
     def __init__(self, initial_balance: float = 0) -> None:
-    self.account_balance = float(initial_balance)
+        self.account_balance = float(initial_balance)
 
-def deposit(self, amount):
-    if amount <= 0:
-        raise ValueError("Deposit amount must be positive.")  
-    self.account_balance += float(amount)
+    def deposit(self, amount):
+        if amount <= 0:
+            raise ValueError("Deposit amount must be positive.") 
+        self.account_balance += float(amount)
 
 def withdraw(self, amount):
     if amount <= 0 :
