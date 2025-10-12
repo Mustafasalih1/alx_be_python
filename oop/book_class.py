81270c2e2209cac88bab92022150ba40db91e7dc
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-10-12 22:13:07 +0300

    Add oop

---
+++ oop/book_class.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, "title":str, "author":str, year:int):
+        self.title = tiltle
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f"(title) by (author), published in (year)"
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+    def __del__(self):
+        return f"Deleting (title of the book)"
