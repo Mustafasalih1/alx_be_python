5f5b14bde80535bcbdc698dd2d8580d433ff4462
mustafa mohamedelhassan <eng.mustafasalih1@gmail.com>
2025-10-12 22:39:03 +0300

    ADD

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,10 +5,10 @@ class Book:
         self.year = year
 
     def __str__(self):
-        return f"(title) by (author), published in (year)"
+        return f"{self.title} by {self.author}, published in {year}"
 
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
     def __del__(self):
-        return f"Deleting (title of the book)"
+        print(f"Deleting {self.title}")
